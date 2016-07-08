1935b29a2b0d15e26df6f0e99a6657608ecaed35
Jose Antonio Martin H <joseantonio.martinhernandez@telefonica.com>
2016-07-08 09:52:34 +0200

    Testing new rule gain value

--- lrdt/dt_transformer.py
+++ lrdt/dt_transformer.py
@@ -212,7 +212,7 @@ class DTRTransformer(BaseEstimator, TransformerMixin):
             _X_copy[k] = 0.0
             # get indices of samples fullining the rule
             ind = _X_copy.query(rule['rule']).index
-            _X_copy.loc[ind, k] = 1.0
+            _X_copy.loc[ind, k] = 2 * (rule['class'] - 0.5) * rule['probability']
             # _X_copy.loc[ind, 'rule_class'] = 2 * (rule['class'] - 0.5)
             self.posterior.loc[ind, 'class'] = rule['class']
             self.posterior.loc[ind, 'p'] = rule['probability']
